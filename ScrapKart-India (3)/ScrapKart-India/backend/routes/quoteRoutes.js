6d984aa3b32ceaa3d3e0a62800454aa3282d7825
anshvarshney014-coder <anshvarshney014@gmail.com>
2026-09-16 23:42:43 +0530

    Delete ScrapKart-India (3)/ScrapKart-India directory

--- ScrapKart-India (3)/ScrapKart-India/backend/routes/quoteRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createQuote, getQuotes } from "../controllers/quoteController.js";
-
-const router = Router();
-
-router.post("/", createQuote);
-router.get("/", getQuotes);
-
-export default router;
